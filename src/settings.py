49cadd1dc7dfe637c96dfec5108f3ad074b8e289
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-27 19:35:15 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -55,6 +55,8 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = {"license plate": [127, 255, 0]}
 PAPER: Optional[str] = None
 CITATION_URL: Optional[str] = None
 AUTHORS: Optional[List[str]] = ["Andrew Maranhão"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://www.kaggle.com/andrewmvd"]
+
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -102,6 +104,7 @@ def get_settings():
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS    
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
